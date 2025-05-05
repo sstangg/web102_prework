c6764c34d923cb3ee15e2623670220dbc5e4dd80
sstangg <sstang@bu.edu>
2025-05-04 20:33:07 -0400

    completed pdf quests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # WEB102 Prework - *Name of App Here*
 
-Submitted by: **Your Name Here**
+Submitted by: **Sophia Tang**
 
 **Name of your app** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
 
@@ -10,10 +10,10 @@ Time spent: **X** hours spent in total
 
 The following **required** functionality is completed:
 
-* [ ] The introduction section explains the background of the company and how many games remain unfunded.
-* [ ] The Stats section includes information about the total contributions and dollars raised as well as the top two most funded games.
-* [ ] The Our Games section initially displays all games funded by Sea Monster Crowdfunding
-* [ ] The Our Games section has three buttons that allow the user to display only unfunded games, only funded games, or all games.
+* [X] The introduction section explains the background of the company and how many games remain unfunded.
+* [X] The Stats section includes information about the total contributions and dollars raised as well as the top two most funded games.
+* [X] The Our Games section initially displays all games funded by Sea Monster Crowdfunding
+* [X] The Our Games section has three buttons that allow the user to display only unfunded games, only funded games, or all games.
 
 The following **optional** features are implemented:
 
@@ -23,22 +23,19 @@ The following **optional** features are implemented:
 
 Here's a walkthrough of implemented features:
 
-<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src="./Screen Recording 2025-05-04 202511.gif](Screen%20Recording%202025-05-04%20202511.gif" title='GIF Walkthrough' width='200' alt='GIF Walkthrough' />
 
-<!-- Replace this with whatever GIF tool you used! -->
-GIF created with ...  
-<!-- Recommended tools:
-[Kap](https://getkap.co/) for macOS
-[ScreenToGif](https://www.screentogif.com/) for Windows
-[peek](https://github.com/phw/peek) for Linux. -->
+GIF created with FreeConvert https://www.freeconvert.com/convert/video-to-gif/download
 
 ## Notes
 
 Describe any challenges encountered while building the app.
 
+Learning how to use new functions like filter, reduce, and sort.
+
 ## License
 
-    Copyright [yyyy] [name of copyright owner]
+    Copyright 2025 Sophia Tang
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- index.js
+++ index.js
@@ -29,27 +29,44 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
+    for (let i = 0; i < games.length; i++) {
 
         // create a new div element, which will become the game card
-
+        const card = document.createElement("div");
 
         // add the class game-card to the list
-
+        card.classList.add("game-card");
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
+        card.innerHTML = `
+            <h4>${games[i].name}</h4>
+            <h5>${games[i].description}<br/><br/>
+                Pledged: $${games[i].pledged}<br/>
+                Goal: $${games[i].goal}<br/>
+                Backers: ${games[i].backers}<br/>
+            </h5>
+            <img src="${games[i].img}" class="game-img" alt="game image ${games[i].name}" />
+        `;
+        /*
+            "name": "Heroes Of Mythic Americas",
+            "description": "An exciting 5e RPG supplement that heroically represents pre-Columbian American cultures and mythologies",
+            "pledged": 1572,
+            "goal": 10000,
+            "backers": 9,
+            "img": "./assets/heroes_of_mythic_americas.png"
+         */
 
         // append the game to the games-container
-
+        gamesContainer.append(card);
+    }
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-
+addGamesToPage(GAMES_JSON);
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
@@ -61,20 +78,31 @@ function addGamesToPage(games) {
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce((acc, game) => {
+    return acc + game.backers; // acc = summed up backers @ that point, game.backers = GAMES_JSON[i].backers
+}, 0); // initial value = 0
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = totalContributions.toLocaleString('en-US');
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
-// set inner HTML using template literal
+const totalRaised = GAMES_JSON.reduce((acc, game) => {
+    return acc + game.pledged;
+}, 0); // initial value = 0
 
+// set inner HTML using template literal
+raisedCard.innerHTML = totalRaised.toLocaleString('en-US');
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
 
+const totalGames = GAMES_JSON.reduce((acc) => {
+    return acc + 1;
+}, 0); // initial value = 0
+
+gamesCard.innerHTML = totalGames.toLocaleString('en-US');
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
@@ -87,10 +115,12 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    let listOfUnfundedGames = GAMES_JSON.filter((game) => {
+        return game.pledged < game.goal; // if $pledged < $goal, then it hasn't been funded
+    });
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(listOfUnfundedGames);
 }
 
 // show only games that are fully funded
@@ -98,10 +128,12 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    let listOfFundedGames = GAMES_JSON.filter((game) => {
+        return game.pledged >= game.goal; // if $pledged >= $goal, then it has been funded
+    });
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(listOfFundedGames);
 }
 
 // show all games
@@ -109,7 +141,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -118,7 +150,15 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+unfundedBtn.addEventListener("click", function(event) {
+    filterUnfundedOnly();
+})
+fundedBtn.addEventListener("click", function(event) {
+    filterFundedOnly();
+})
+allBtn.addEventListener("click", function(event) {
+    showAllGames();
+})
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
@@ -129,12 +169,23 @@ const allBtn = document.getElementById("all-btn");
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const numUnfundedGames = GAMES_JSON.reduce((count, game) => {
+    return game.pledged < game.goal ? count + 1 : count; // if unfunded, +1. +0 if funded.
+}, 0);
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+const displayStr = `A total of ${totalRaised.toLocaleString()} has been raised for ${totalGames.toLocaleString()} games. `
+                            +  (numUnfundedGames !== 1 ?
+                            `Currently, ${numUnfundedGames} games remain unfunded. `: // >=2 or 0 games
+                            `Currently, ${numUnfundedGames} game remains unfunded. `) // 1 game
+                            +  (numUnfundedGames > 0 ?
+                            `We need your help to fund these amazing games!`: // add if games need funding
+                            ``);
 
 // create a new DOM element containing the template string and append it to the description container
+const descriptionP = document.createElement("p");
+descriptionP.innerHTML = displayStr;
+descriptionContainer.appendChild(descriptionP);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
@@ -149,7 +200,13 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+let [first, second, ...others] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
-
+const nameFirst = document.createElement("p");
+nameFirst.innerHTML = first.name;
+firstGameContainer.appendChild(nameFirst);
 // do the same for the runner up item
+const nameSecond = document.createElement("p");
+nameSecond.innerHTML = second.name;
+secondGameContainer.appendChild(nameSecond);
